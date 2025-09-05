cb1e1318a3423476cf0a3cb43e278220a284a4b0
AbeEf <afulop347@gmail.com>
2025-09-05 01:07:10 -0400

    updated mock data

--- js/env.js
+++ js/env.js
@@ -6,5 +6,4 @@
 export const API_BASE_URL = "https://localhost:3000";
 
 // Query flag to enable mock mode (?mock=1)
-export const MOCK_MODE = true ||
-  new URLSearchParams(window.location.search).get("mock") === "1";
+export const MOCK_MODE = true;
